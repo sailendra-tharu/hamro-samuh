96ac652c6eaac3ad783626bd37c5873db56edb30
sailendra-tharu <sailendradastharu2000@gmail.com>
2026-07-28 17:23:07 +0545

    design of samuh

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://hamrosamuh.netlify.app',
+      dynamicRoutes: ['/about', '/services', '/members', '/contact', '/terms'],
+    }),
+  ],
 })
